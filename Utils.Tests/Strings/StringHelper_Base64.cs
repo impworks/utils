ae05a66f0c3be0af9aab777d2eae76882475e0bc
Andrei Kurosh <mail@spark.ms>
2022-12-21 12:47:40 +0300

    Base64 string-to-string encoding methods.

---
+++ Utils.Tests/Strings/StringHelper_Base64.cs
@@ -0,0 +1,27 @@
+﻿using Impworks.Utils.Strings;
+using NUnit.Framework;
+
+namespace Utils.Tests.Strings
+{
+    [TestFixture]
+    public class StringHelper_Base64
+    {
+        [Test]
+        public void Encode()
+        {
+            var src = "Hello world";
+            var encoded = StringHelper.Base64Encode(src);
+
+            Assert.AreEqual("SGVsbG8gd29ybGQ=", encoded);
+        }
+
+        [Test]
+        public void Decode()
+        {
+            var src = "SGVsbG8gd29ybGQ=";
+            var encoded = StringHelper.Base64Decode(src);
+
+            Assert.AreEqual("Hello world", encoded);
+        }
+    }
+}
